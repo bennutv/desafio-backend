d3f79ee2218d9beb3a62c31d05db82126ad1f206
miquelis <raphaelmiquelis@gmail.com>
2019-11-28 01:46:25 -0300

    Criação da extrutura do projeto, listagem dos dados e autenticação jwt

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require("./router/auth")(app);
+require("./router/noticiais")(app);
+
+let port = 3000;
+app.listen(port, () => {
+  console.log(`API Rodando na porta ${port}`);
+});

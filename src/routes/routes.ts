eaf3bf31ef6de1409e41a06e34fb55b374412fe4
asda <sadasdada@sdfasdas.con>
2022-08-06 23:38:47 -0300

    🔨chore: Setup Login and Logout

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-
+import { AuthController } from '../controllers/auth';
+import { jwtAuth } from '../middleware/auth';
 
 
 const routes = Router();
 
 routes.get('/', (request, response) => response.json({ message: 'Desafio Backend' }));
-
+routes.post('/login', new AuthController().login)
+routes.post('/logout', jwtAuth, new AuthController().logout)
 
 export { routes };

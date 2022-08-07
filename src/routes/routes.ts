cc18ff2ada6ad89b342c594fa87f9646f033a84f
asda <sadasdada@sdfasdas.con>
2022-08-06 23:40:35 -0300

    🔨chore: Setup News List and List by id

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth';
+import { NewsController } from '../controllers/news';
 import { jwtAuth } from '../middleware/auth';
 
 
@@ -8,5 +9,7 @@ const routes = Router();
 routes.get('/', (request, response) => response.json({ message: 'Desafio Backend' }));
 routes.post('/login', new AuthController().login)
 routes.post('/logout', jwtAuth, new AuthController().logout)
+routes.get('/news', jwtAuth ,new NewsController().news)
+routes.get('/news/:id', jwtAuth, new NewsController().newsItem)
 
 export { routes };

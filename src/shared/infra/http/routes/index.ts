e776a9701ca0c579657f4b9c885614e7a2483b96
Felipe Rosa <felipesrosa@gmail.com>
2022-09-17 22:29:04 -0300

    feat: create news route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
 import { accountsRoutes } from "./accounts.routes";
+import { newsRoutes } from "./news.routes";
 
 const router = Router();
 router.use("/accounts", accountsRoutes);
+router.use("/news", newsRoutes);
 
 export { router };
